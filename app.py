4beaab751bb495f02cc62aab8d2ce2ce11bd7324
Krrish-001 <krrizunangalia@gmail.com>
2026-04-11 00:50:07 +0530

    make UI dynamic

--- app.py
+++ app.py
@@ -1,38 +1,18 @@
 from flask import Flask, render_template
-import mysql.connector
 
 app = Flask(__name__)
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host="db",
-        user="root",
-        password="root",
-        database="netflix"
-    )
+# Fake movie data (later we replace with API/DB)
+movies = [
+    {"title": "Movie 1", "image": "https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"},
+    {"title": "Movie 2", "image": "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"},
+    {"title": "Movie 3", "image": "https://image.tmdb.org/t/p/w500/6agKYU5IQFpuDyUYPu39w7UCRrJ.jpg"},
+    {"title": "Movie 4", "image": "https://image.tmdb.org/t/p/w500/rTh4K5uw9HypmpGslcKd4QfHl93.jpg"},
+]
 
-@app.route('/')
+@app.route("/")
 def home():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("CREATE TABLE IF NOT EXISTS visits (count INT)")
-    cursor.execute("SELECT count FROM visits")
-
-    result = cursor.fetchone()
-
-    if result is None:
-        cursor.execute("INSERT INTO visits (count) VALUES (1)")
-        count = 1
-    else:
-        count = result[0] + 1
-        cursor.execute("UPDATE visits SET count=%s", (count,))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return render_template("index.html", count=count)
+    return render_template("index.html", movies=movies)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
