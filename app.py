426b49eb152a66d0a82a1d32a7b7f19c9999b689
Krrish-001 <krrizunangalia@gmail.com>
2026-04-11 00:58:34 +0530

    UI redesign netflix clean

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 # Fake movie data (later we replace with API/DB)
 movies = [
-    {"title": "Movie 1", "image": "https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"},
-    {"title": "Movie 2", "image": "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"},
-    {"title": "Movie 3", "image": "https://image.tmdb.org/t/p/w500/6agKYU5IQFpuDyUYPu39w7UCRrJ.jpg"},
-    {"title": "Movie 4", "image": "https://image.tmdb.org/t/p/w500/rTh4K5uw9HypmpGslcKd4QfHl93.jpg"},
+    {"title": "Wonder Woman", "image": "https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"},
+    {"title": "Dark Knight", "image": "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"},
+    {"title": "Borat", "image": "https://image.tmdb.org/t/p/w500/6agKYU5IQFpuDyUYPu39w7UCRrJ.jpg"},
+    {"title": "Cruella", "image": "https://image.tmdb.org/t/p/w500/rTh4K5uw9HypmpGslcKd4QfHl93.jpg"},
 ]
 
 @app.route("/")
